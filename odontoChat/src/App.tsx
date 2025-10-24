e8bab9afcc358ee347994c9d7036159d070eaf1d
dubersantiago <dubersi1@gamil.com>
2025-10-24 14:54:24 -0500

    feat: contadores

--- odontoChat/src/App.tsx
+++ odontoChat/src/App.tsx
@@ -5,8 +5,11 @@ function App() {
 
   return (
     <>
-      <h1 className='text-5xl text-amber-700'>titulo de prueba con jenkins {count}</h1>
-      <button onClick={() => setCount(count + 1)}>Incrementar</button>
+      <h1 className='text-5xl text-amber-700 w-full text-center mt-2'>titulo de prueba con jenkins {count}</h1>
+      <div className='w-full flex justify-center aling-items-center gap-4 mt-4'>
+        <button className='bg-cyan-500 rounded-2xl hover: cursor-pointer p-2' onClick={() => setCount(count + 1)}>Incrementar</button>
+        <button className='bg-cyan-500 rounded-2xl hover: cursor-pointer p-2' onClick={() => setCount(count - 1)}>Decrementar</button>
+      </div>
     </>
   )
 }
